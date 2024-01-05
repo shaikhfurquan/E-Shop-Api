a8991bda95c619292ad56e1f0b5aeb7934b0ba7b
shaikhfurquan <shaikhfurquan313@gmail.com>
2024-01-05 13:44:24 +0530

    jwt

--- helper/jwt.js
+++
@@ -1,9 +0,0 @@
-import {expressjwt} from "express-jwt";
-
-function authJwt() {
-    const secret = process.env.SEC_KEY;
-    console.log(secret);
-    return expressjwt({ secret, algorithms: ['HS256'] });
-}
-
-export default authJwt
